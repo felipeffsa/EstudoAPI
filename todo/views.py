8c362719c8f84146b0de3e3ad37969e099488de0
Felipe <felipeffsa@hotmail.com>
2024-02-23 11:23:41 -0300

    Terceiro modo de escrever as classes

--- todo/views.py
+++ todo/views.py
@@ -6,41 +6,17 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
+from rest_framework import generics
 
 
 
-
-class PessoaListAndCreate(APIView):
-    def get(self,request):
-        pessoas = Pessoa.objects.all()
-        serializer = PessoaSerializer(pessoas, many=True)
-        return Response(serializer.data)
-    def post(self,request):
-        serializer = PessoaSerializer(data =request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
+class PessoaListAndCreate(generics.ListCreateAPIView):
+    queryset = Pessoa.objects.all()
+    serializer_class = PessoaSerializer
+     
         
-        
-class PessoaDetailChangeAndDelete(APIView):
-    def get_object(self, pk):
-        try:
-           return Pessoa.objects.get(pk = pk)
-        except Pessoa.DoesNotExist:
-            raise NotFound()
-    def get(self,request, pk):
-        pessoa = self.get_object(pk)
-        serializer = PessoaSerializer(pessoa)
-        return Response(serializer.data)
-    def put(self,request, pk):
-        pessoa = self.get_object(pk)
-        serializer = PessoaSerializer(pessoa, data =request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-    def delete(self, request, pk):
-        pessoa = self.get_object(pk)
-        pessoa.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
+class PessoaDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pessoa.objects.all()
+    serializer_class = PessoaSerializer
+
 
